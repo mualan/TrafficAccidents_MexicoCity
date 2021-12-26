628453e90982c3d9731720d041feb08f05efc385
Alan <e.alanmuro@gmail.com>
2021-12-26 19:38:08 +0100

    Adding master.R code

---
+++ analysis_master.R
@@ -0,0 +1,153 @@
+
+
+##########################################
+##########################################
+#####
+#####  Mexico city incidents - Analysis - Master file
+#####  
+#####
+##########################################
+##########################################
+
+
+# Questions - confirmed incidents
+
+# Number of incidents are going down with years. Does this represent 
+# the reality?
+
+
+
+
+
+# Donde hay mas incidentes? a que horas del dia, dia de semanam etc? tiene que ver con la fiesta?
+# Predict: numbero de incidentes en un dia particular, en la noche, de la semana en una delegacion
+# where "supposetly"do the false incidents occur?
+# which area of the city/delegstion hass the most detentions/deaths
+
+# Sources: 
+#https://datos.cdmx.gob.mx/dataset/incidentes-viales-c5
+
+##### Settings
+#####
+#####
+rm(list=ls())
+Sys.setenv(LANG = "en")
+options( scipen = 20 )
+dev.off()
+
+getwd()
+setwd("/Users/Muro/Documents/DS/Project_MexicoCityAccidents")
+
+### Packages
+#install.packages("esquisse")
+
+library(dplyr)
+library(forcats) #fct_relevels
+
+library(readr) #read_csv, parse_number (drop non-numeric characters before or after first number)
+library(lubridate) # dates, times
+library(DataExplorer)
+
+library(naniar) #missing values visualization and investigation
+library(stringr) #str_replace
+
+library(ggmap)
+
+library(esquisse)
+
+
+
+##### Useful
+#####
+#####################################
+######################################
+#cat(paste0(levels(dt$incident_reporttype),"="), sep="\n")
+
+
+#####################################
+######################################             
+##### Data preparation
+#####
+#####################################
+######################################
+
+source("datapreparation.R")
+
+#####################################
+######################################             
+##### Dataset 1: confirmed incidents
+#####
+#####################################
+######################################
+
+
+
+
+#table(dt$incident_code)
+# All received incidents (all incident codes)
+# a = emergency unit sent and incident confirmed (real incidents)
+# b = emergency unit sent but incident could not be confirmed
+# i = information requests
+# f = incident initially reported was confimed to be fake
+# d = duplicated reports
+
+
+# Confirmed incidents
+dtc <- dt %>% filter( incident_code == "a") %>% 
+        select(-"report_date_orig", -"report_time_orig" )
+
+
+##### EDA 
+#####
+#####################################
+#################¬#####################
+
+### Fast EDA
+###
+
+colnames(dtc)
+str(dtc)
+summary(dtc)
+https://yuzar-blog.netlify.app/posts/2021-01-09-exploratory-data-analysis-and-beyond-in-r-in-progress/
+#create_report(dtc) #without response variables
+
+
+
+### Map
+###
+
+### Plotting on map
+cdmx <- get_stamenmap(
+        bbox = c(left = -99.28, 
+                 bottom = 19.25, 
+                 right = -99.00, 
+                 top = 19.58), 
+        maptype = "toner",
+        zoom = 12)
+
+ggmap(cdmx) +
+        geom_point(data = dtc ,
+                   aes(x = long, y= lat, color = injured_flag),
+                   alpha = 1/10, size = 1/2) +
+        facet_wrap(report_weekday ~ .) + 
+        guides(colour = guide_legend(override.aes = list(size=10)))
+
+
+ggmap(cdmx) +
+        geom_point(data = dtc ,
+                   aes(x = long, y= lat, color = injured_flag),
+                   alpha = 1/10, size = 1/2) +
+        facet_wrap( report_year ~ .)  + 
+        guides(colour = guide_legend(override.aes = list(size=10)))
+
+ggplot(dtc) + geom_histogram(aes(report_year, fill = injured_flag),
+                             stat="count") 
+ 
+
+
+
+
+
+
+
+
